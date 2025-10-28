6883b23c3e296968af3c47b534888970313dddb9
NALLA933 <piyush960yogi@gmail.com>
2025-10-28 14:16:56 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = "-1002191083108"
     TOKEN = "7891572866:AAEKgMqTNK0vQ_mAw63YFKdL6bD2oEiss14"
     mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/SMDBOTZ-10-25-2", "https://telegra.ph/SMDBOTZ-10-25-3"]
+    PHOTO_URL = ["https://files.catbox.moe/8722ku.jpeg", "https://files.catbox.moe/kgcrnb.jpeg"]
     SUPPORT_CHAT = "PICK_X_SUPPORT"
     UPDATE_CHAT = "PICK_X_UPDATE"
     BOT_USERNAME = "waifukunbot"
