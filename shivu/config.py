9a88c2aa59511f20529f4c108dd7557a0e66c68f
NALLA933 <piyush960yogi@gmail.com>
2025-10-28 17:03:57 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,7 +4,7 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "8420981179"
     sudo_users = ["8297659126", "8420981179", "5147822244", "7843303499", "7435049371", "6863917190", "1201153141", "7410843798"]
-    GROUP_ID = "-1002191083108"
+    GROUP_ID = "-1003150808065"
     TOKEN = "7891572866:AAEKgMqTNK0vQ_mAw63YFKdL6bD2oEiss14"
     mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://files.catbox.moe/8722ku.jpeg", "https://files.catbox.moe/kgcrnb.jpeg"]
