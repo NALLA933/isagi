cef2e31c1d726c74262c2b2dc4098c2d81573832
NALLA933 <piyush960yogi@gmail.com>
2025-12-21 16:01:17 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,24 +1,30 @@
 class Config(object):
     LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "8420981179"
-    sudo_users = ["8297659126", "8420981179", "5147822244", "6863917190", "6416551017", "6110294479", "8064384504", "8133762943"]
-    GROUP_ID = "-1003150808065"
+    # IDs hamesha bina quotes ke (Integer) honi chahiye
+    OWNER_ID = 8420981179
+    
+    # Sudo users ki list mein bhi numbers rakhein
+    sudo_users = [8297659126, 8420981179, 5147822244, 6863917190, 6416551017, 6110294479, 8064384504, 8133762943]
+    
+    # Group IDs hamesha negative number se start hoti hain
+    GROUP_ID = -1003150808065
+    CHARA_CHANNEL_ID = -1003154253941
+    
     TOKEN = "7891572866:AAEKgMqTNK0vQ_mAw63YFKdL6bD2oEiss14"
     mongo_url = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
+    
     PHOTO_URL = ["https://files.catbox.moe/8722ku.jpeg", "https://files.catbox.moe/kgcrnb.jpeg"]
     SUPPORT_CHAT = "PICK_X_SUPPORT"
     UPDATE_CHAT = "PICK_X_UPDATE"
     BOT_USERNAME = "waifukunbot"
-    CHARA_CHANNEL_ID = "-1003154253941"
-    api_id = "21705508"
+    
+    # API Hash string hi rehta hai, lekin API ID number hota hai
+    api_id = 21705508
     api_hash = "1d590f4c3d2029a7ef7df087707d7441"
 
-    
 class Production(Config):
     LOGGER = True
-
 
 class Development(Config):
     LOGGER = True
